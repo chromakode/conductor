e1df507d1228a4e53a64987af5aac4a3d2e78038
chromakode <chromakode@gmail.com>
2008-11-24 15:53:34 -0800

    Change prospective API format

--- src/tests/sampleplays.py
+++ src/tests/sampleplays.py
@@ -1,7 +1,14 @@
+from itertools import izip
+
 from conductor.engine import conductor
 
 c = conductor.Conductor("conductor-test.db")
 
-c.track_played(c.songdb.get_track("Blue", "Cold", "Colors"), after=c.songdb.get_track("Cyan", "Cold", "Colors"))
-c.track_played(c.songdb.get_track("Cyan", "Cold", "Colors"), after=c.songdb.get_track("Green", "Cold", "Colors"))
-c.track_played(c.songdb.get_track("Green", "Cold", "Colors"), after=c.songdb.get_track("Purple", "Cold", "Colors"))
+
+tracks = [{"name": "Blue",   "album": "Cold", "artist": "Colors"},
+          {"name": "Cyan",   "album": "Cold", "artist": "Colors"},
+          {"name": "Green",  "album": "Cold", "artist": "Colors"},
+          {"name": "Purple", "album": "Cold", "artist": "Colors"}]
+
+for cur, prev in izip(tracks, tracks[1:]):
+    c.track_played(cur, previous = prev)
