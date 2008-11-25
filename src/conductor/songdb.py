159283eb32ea8e60299d5f646a543f1a7e5e7676
chromakode <chromakode@gmail.com>
2008-11-25 00:35:36 -0800

    Simple database retrieval code complete.

--- src/conductor/songdb.py
+++ src/conductor/songdb.py
@@ -56,85 +56,75 @@ class SongDB:
                 )""")
             
             # TODO: add indexes here?
-    
-    def get_artist(self, artistname, add=False):
+            
+    def _get_thing_id(self, selectsql, insertsql, add, **kwargs):
         with self.db:
-            result = self.db.execute("""
-                SELECT artistid FROM artists
-                WHERE name = ?
-                """, (artistname,)).fetchone()
+            result = self.db.execute(selectsql, kwargs).fetchone()
             
             if result:
-                artistid = result[0]
+                id = result[0]
             else:
                 if add:
-                    artistid = self.db.execute("""
-                        INSERT INTO artists (name) VALUES (?)
-                        """, (artistname,)).lastrowid
+                    id = self.db.execute(insertsql, kwargs).lastrowid
                 else:
                     return None
-        
-        return Artist(artistid, artistname)
-    
-    
-    def get_album(self, albumname, add=False):
-        with self.db:
-            result = self.db.execute("""
-                SELECT albumid FROM albums
-                WHERE name = ?
-                """, (albumname,)).fetchone()
+       
+        return id
             
-            if result:
-                albumid = result[0]
-            else:
-                if add:
-                    albumid = self.db.execute("""
-                        INSERT INTO albums (name) VALUES (?)
-                        """, (albumname,)).lastrowid
-                else:
-                    return None
-        
-        return Album(albumid, albumname)
+    def get_artist(self, artist_name, add=False):
+        artist_id = self._get_thing_id("SELECT artistid FROM artists WHERE name=:name",
+                                       "INSERT INTO artists (name) VALUES (:name)",
+                                       add, name=artist_name)
+        if artist_id:
+            return Artist(artist_id, artist_name)
     
-    def get_track(self, trackname, albumname, artistname, add=False):
-        album = self.get_album(albumname, add)
+    
+    def get_album(self, album_name, add=False):
+        album_id = self._get_thing_id("SELECT albumid FROM albums WHERE name=:name",
+                                      "INSERT INTO albums (name) VALUES (:name)", 
+                                      add, name=album_name)
+        if album_id:
+            return Album(album_id, album_name)
+        
+    def get_genre(self, genre_name, add=False):
+        genre_id = self._get_thing_id("SELECT genreid FROM genres WHERE name=:name",
+                                      "INSERT INTO genres (name) VALUES (:name)",
+                                      add, name=genre_name)
+        if genre_id:
+            return Genre(genre_id, genre_name)
+           
+    def get_track(self, track_name, album_name, artist_name, genre_name=None, add=False):
+        album = self.get_album(album_name, add)
         if not album:
             return None
         
-        artist = self.get_artist(artistname, add)
+        artist = self.get_artist(artist_name, add)
         if not artist:
             return None
         
-        with self.db:
-            result = self.db.execute("""
-                SELECT trackid FROM tracks
-                WHERE name = ? AND albumid = ? AND artistid = ?
-                """, (trackname, album.id, artist.id)).fetchone()
-            
-            if result:
-                trackid = result[0]
-            else:
-                if add:
-                    trackid = self.db.execute("""
-                        INSERT INTO tracks (name, albumid, artistid) VALUES (?, ?, ?)
-                        """, (trackname, album.id, artist.id)).lastrowid
-                else:
-                    return None
+        if genre_name:
+            genre = self.get_genre(genre_name, add)
+            if not genre:
+                return None
         
-        return Track(trackid, album, album.artist)
-        
-class Artist:
+        track_id = self._get_thing_id("SELECT trackid FROM tracks WHERE name = :name AND albumid = :album_id AND artistid = :artist_id",
+                                      "INSERT INTO tracks (name, albumid, artistid, genreid) VALUES (:name, :album_id, :artist_id, :genre_id)", 
+                                      add, name=track_name, album_id=album.id, artist_id=artist.id, genre_id=(genre.id if genre_name else None))
+        if track_id:
+            return Track(track_id, track_name, album, artist, genre)
+
+class Thing:
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
-class Album:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-class Track:
-    def __init__(self, id, name, album, artist):
-        self.id = id
-        self.name = name
+class Artist(Thing): pass
+class Album(Thing): pass
+class Genre(Thing): pass
+
+class Track(Thing):
+    def __init__(self, id, name, album, artist, genre):
+        Thing.__init__(self, id, name)
         self.album = album
         self.artist = artist
+        self.genre = genre
