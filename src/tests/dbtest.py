9b70e21be59f098ed461748d65ebe239b34c8d49
chromakode <chromakode@gmail.com>
2008-12-03 19:20:27 -0800

    Improve the formatting of dbtest.

--- src/tests/dbtest.py
+++ src/tests/dbtest.py
@@ -8,17 +8,26 @@ def main(args):
     db = musicdb.MusicDB("/tmp/musicdb-test.db")
     db.load()
     
+    print "Requesting album \"AL-1\"..." 
     al1 = db.get_album("AL-1", True)
     print al1.__dict__
+    print
     
+    print "Requesting track \"Track-1\" from \"AL-1\" by \"Test\"..." 
     t1 = db.get_track("Track-1", "AL-1", "AR-1", "Test", add=True)
     print t1.__dict__
+    print
     
+    print "Requesting track \"Track-2\" from \"AL-2\" by \"Test\"..."
     t2 = db.get_track("Track-2", "AL-2", "AR-1", "Test", add=True)
     print t2.__dict__
+    print
     
+    print "Requesting track \"Track-1\" from \"AL-1\" by \"Test\"..."
     t1_2 = db.get_track("Track-1", "AL-1", "AR-1", "Test", add=True)
-    print t1.id == t1_2.id, t1_2.__dict__
+    print "First request id: %s; Second request id: %s" % (t1.id, t1_2.id)
+    print t1_2.__dict__
+    print
     
     db.unload()
     
